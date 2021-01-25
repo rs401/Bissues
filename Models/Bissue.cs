74ee1a08758cf4b1803b5dd2d109cb7ffa41268a
BlackBeard <rich.stadnick@gmail.com>
2021-01-25 13:17:28 -0500

    Added IsOpen property to Bissue, Forgot default.

--- Models/Bissue.cs
+++ Models/Bissue.cs
@@ -12,7 +12,7 @@ namespace Bissues.Models
         public string Title { get; set; }
         [Required]
         public string Description { get; set; }
-        public bool IsOpen { get; set; }
+        public bool IsOpen { get; set; } = true;
         public ICollection<Message> Messages { get; set; }
 
         /* FK Owner of the Bissue */
