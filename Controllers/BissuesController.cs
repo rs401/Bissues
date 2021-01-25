a15b8d0543352b59cd1858c058e799cd5dd1a92b
BlackBeard <rich.stadnick@gmail.com>
2021-01-25 09:59:23 -0500

    Added controllers and CRUD views

---
+++ Controllers/BissuesController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bissues.Data;
+using Bissues.Models;
+
+namespace Bissues.Controllers
+{
+    public class BissuesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BissuesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Bissues
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Bissues.Include(b => b.Project);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Bissues/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bissue = await _context.Bissues
+                .Include(b => b.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bissue == null)
+            {
+                return NotFound();
+            }
+
+            return View(bissue);
+        }
+
+        // GET: Bissues/Create
+        public IActionResult Create()
+        {
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name");
+            return View();
+        }
+
+        // POST: Bissues/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,AppUserId,ProjectId,CreatedDate,ModifiedDate")] Bissue bissue)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(bissue);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name", bissue.ProjectId);
+            return View(bissue);
+        }
+
+        // GET: Bissues/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bissue = await _context.Bissues.FindAsync(id);
+            if (bissue == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name", bissue.ProjectId);
+            return View(bissue);
+        }
+
+        // POST: Bissues/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,AppUserId,ProjectId,CreatedDate,ModifiedDate")] Bissue bissue)
+        {
+            if (id != bissue.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(bissue);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BissueExists(bissue.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProjectId"] = new SelectList(_context.Projects, "Id", "Name", bissue.ProjectId);
+            return View(bissue);
+        }
+
+        // GET: Bissues/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var bissue = await _context.Bissues
+                .Include(b => b.Project)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (bissue == null)
+            {
+                return NotFound();
+            }
+
+            return View(bissue);
+        }
+
+        // POST: Bissues/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var bissue = await _context.Bissues.FindAsync(id);
+            _context.Bissues.Remove(bissue);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BissueExists(int id)
+        {
+            return _context.Bissues.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Bissues.Data;
+using Bissues.Models;
+
+namespace Bissues.Controllers
+{
+    public class MessagesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MessagesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Messages
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Messages.Include(m => m.Bissue);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Messages/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Messages
+                .Include(m => m.Bissue)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // GET: Messages/Create
+        public IActionResult Create()
+        {
+            ViewData["BissueId"] = new SelectList(_context.Bissues, "Id", "Description");
+            return View();
+        }
+
+        // POST: Messages/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Body,AppUserId,BissueId,CreatedDate,ModifiedDate")] Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(message);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BissueId"] = new SelectList(_context.Bissues, "Id", "Description", message.BissueId);
+            return View(message);
+        }
+
+        // GET: Messages/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Messages.FindAsync(id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+            ViewData["BissueId"] = new SelectList(_context.Bissues, "Id", "Description", message.BissueId);
+            return View(message);
+        }
+
+        // POST: Messages/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Body,AppUserId,BissueId,CreatedDate,ModifiedDate")] Message message)
+        {
+            if (id != message.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(message);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MessageExists(message.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["BissueId"] = new SelectList(_context.Bissues, "Id", "Description", message.BissueId);
+            return View(message);
+        }
+
+        // GET: Messages/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var message = await _context.Messages
+                .Include(m => m.Bissue)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (message == null)
+            {
+                return NotFound();
+            }
+
+            return View(message);
+        }
+
+        // POST: Messages/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var message = await _context.Messages.FindAsync(id);
+            _context.Messages.Remove(message);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MessageExists(int id)
+        {
+            return _context.Messages.Any(e => e.Id == id);
+        }
+    }
+}
