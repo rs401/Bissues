7711b1da9e5f7aa9e63bdc30bb21a6d2258138aa
BlackBeard <rich.stadnick@gmail.com>
2021-01-25 09:08:41 -0500

    Add BaseEntity for timestamps

---
+++ Models/BaseEntity.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Bissues.Models
+{
+    public class BaseEntity
+    {
+        public DateTime CreatedDate { get; set; }
+        public DateTime ModifiedDate { get; set; }
+    }
+}
