e23a3320b30523ea8fb1826ed01161981d81ccbd
Rich Stadnick <rich.stadnick@gmail.com>
2021-03-04 11:13:57 -0500

    Added ViewModels namespace, IndexViewModel to hold recent Bissues for a list on main Home/Index view.

--- Bissues/Controllers/HomeController.cs
+++ Bissues/Controllers/HomeController.cs
@@ -4,8 +4,12 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Bissues.Models;
+using Bissues.ViewModels;
+using Bissues.Data;
 
 namespace Bissues.Controllers
 {
@@ -15,15 +19,20 @@ namespace Bissues.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            // var bissues = await _context.Bissues.Where(b => b.IsOpen == true).OrderByDescending(bm => bm.ModifiedDate).Take(5);
+            var bissues = await _context.Bissues.Include(b => b.Project).OrderByDescending(b => b.IsOpen).ThenByDescending(b => b.ModifiedDate).Take(5).ToListAsync();
+            var model = new IndexViewModel(){Bissues = bissues};
+            return View(model);
         }
 
         public IActionResult Privacy()
---
+++ Bissues/ViewModels/IndexViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Bissues.Models;
+
+namespace Bissues.ViewModels
+{
+    public class IndexViewModel
+    {
+        public List<Bissue> Bissues { get; set; }
+    }
+}
