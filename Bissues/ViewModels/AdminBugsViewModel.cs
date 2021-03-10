b0abd2df862ae08eabf93aa06f2ca9f4f8fd77f1
Rich Stadnick <rich.stadnick@gmail.com>
2021-03-10 16:23:00 -0500

    Added Bugs to admin area.

---
+++ Bissues/ViewModels/AdminBugsViewModel.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using Bissues.Models;
+
+namespace Bissues.ViewModels
+{
+    public class AdminBugsViewModel
+    {
+        public List<Bissue> Bugs { get; set; }
+    }
+}
