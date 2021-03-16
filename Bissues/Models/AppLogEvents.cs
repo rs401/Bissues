ce4fc599ef9f28c6f43468c644e964d3fb9d207c
Rich Stadnick <rich.stadnick@gmail.com>
2021-03-16 13:23:17 -0400

    Added AppLogEvents class for when I go deeper into logging.

---
+++ Bissues/Models/AppLogEvents.cs
@@ -0,0 +1,14 @@
+
+namespace Bissues.Models
+{
+    internal static class AppLogEvents
+    {
+        internal const int Create = 1000;
+        internal const int Read = 1001;
+        internal const int Update = 1002;
+        internal const int Delete = 1003;
+
+        internal const int Details = 3000;
+        internal const int Error = 3001;
+    }
+}
