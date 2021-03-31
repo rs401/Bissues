3b6bf6e966371336c6769396635923af98564e91
Rich Stadnick <rich.stadnick@gmail.com>
2021-03-30 22:04:11 -0400

    Added model for an OfficialNote.

---
+++ Bissues/Models/OfficialNote.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Bissues.Models
+{
+    /// <summary>
+    /// OfficialNote is a note about a specific commit directly related to the Bissue.
+    /// </summary>
+    public class OfficialNote : BaseEntity
+    {
+        public int Id { get; set; }
+        public int BissueId { get; set; }
+        public Bissue Bissue { get; set; }
+        public string CommitId { get; set; }
+        public string CommitURL { get; set; }
+        public string Note { get; set; }
+    }
+}
