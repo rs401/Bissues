15ff29482f902b2d0c9435eb939fe9985ba85b89
Rich Stadnick <rich.stadnick@gmail.com>
2021-04-06 19:57:11 -0400

    Worked out most of AppUserControllerTests.

--- BissuesTest.UnitTests/AppUserControllerTests.cs
+++ BissuesTest.UnitTests/AppUserControllerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Net.Http;
 using System.Security.Claims;
@@ -89,7 +90,7 @@ namespace BissuesTest.UnitTests
             }
         }
 
-        [Fact(Skip = "Can't properly mock UserManager.")]
+        [Fact]
         public async Task Index_ReturnsAView()
         {
             // Arrange
@@ -119,14 +120,56 @@ namespace BissuesTest.UnitTests
                 .ReturnsAsync(user);
             var mockUser = new Mock<UserManager<AppUser>>(store.Object, null, 
                 null, null, null, null, null, null, null);//.Object;
-            mockUser.Setup( userManager => userManager.FindByIdAsync(It.IsAny<string>()))
-                .ReturnsAsync(new AppUser { Id = name });
+            mockUser.Setup( userManager => userManager.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(user);
             // Act
             // Assert
             using (var context = new ApplicationDbContext(_options))
             {
                 _sut = new AppUserController(_logger, context, mockUser.Object){ControllerContext = concontext};
                 var result = await _sut.Index();
+
+                var viewResult = Assert.IsType<ViewResult>(result);
+            }
+        }
+        [Fact]
+        public async Task Developer_ReturnsAView()
+        {
+            // Arrange
+            var name = "admin@admin.com";
+            var user = new AppUser{
+                Email = "admin@admin.com",
+                UserName = "admin@admin.com",
+                FirstName = "Admin",
+                LastName = "Istrator",
+                DisplayName = "Admin",
+                EmailConfirmed = true
+            };
+
+            var httpContext = new Mock<HttpContext>();
+            httpContext.Setup(m => m.User.Identity.Name)
+                .Returns(name);
+
+            var concontext = new ControllerContext(
+                new ActionContext(
+                    httpContext.Object, 
+                    new Microsoft.AspNetCore.Routing.RouteData(), 
+                    new ControllerActionDescriptor()
+                    ));
+            
+            var store = new Mock<IUserStore<AppUser>>();
+            store.Setup(x => x.FindByNameAsync(name, CancellationToken.None))
+                .ReturnsAsync(user);
+            var mockUser = new Mock<UserManager<AppUser>>(store.Object, null, 
+                null, null, null, null, null, null, null);//.Object;
+            mockUser.Setup( userManager => userManager.FindByNameAsync(It.IsAny<string>()))
+                .ReturnsAsync(user);
+            // Act
+            // Assert
+            using (var context = new ApplicationDbContext(_options))
+            {
+                _sut = new AppUserController(_logger, context, mockUser.Object){ControllerContext = concontext};
+                var result = await _sut.Developer();
 
                 var viewResult = Assert.IsType<ViewResult>(result);
             }
@@ -148,7 +191,55 @@ namespace BissuesTest.UnitTests
                 Assert.Equal("Index", redirectResult.ActionName);
             }
         }
+        [Fact]
+        public async Task ReadNotification_WithNullBissue_RedirectsToIndex()
+        {
+            // Arrange
+            int id = 123;
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var notification = new Notification{ Id = id };
+                context.Notifications.Add(notification);
+                context.SaveChanges();
+            }
+            // Act
+            // Assert
+            using (var context = new ApplicationDbContext(_options))
+            {
+                _sut = new AppUserController(_logger, context, _userManager);
+                var result = await _sut.ReadNotification(id);
 
+                var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+                Assert.Equal("Index", redirectResult.ActionName);
+            }
+        }
+        [Fact]
+        public async Task ReadNotification_RemovesNotification_RedirectsToBissuesDetails()
+        {
+            // Arrange
+            int id = 124;
+            int bid = 321;
+            using (var context = new ApplicationDbContext(_options))
+            {
+                var bissue = new Bissue{ Id = bid };
+                context.Bissues.Add(bissue);
+                var notification = new Notification{ Id = id, BissueId = bid };
+                context.Notifications.Add(notification);
+                context.SaveChanges();
+            }
+            // Act
+            // Assert
+            using (var context = new ApplicationDbContext(_options))
+            {
+                _sut = new AppUserController(_logger, context, _userManager);
+                var result = await _sut.ReadNotification(id);
 
+                var redirectResult = Assert.IsType<RedirectToActionResult>(result);
+                Assert.Equal("Details", redirectResult.ActionName);
+
+                var notification = context.Notifications.Where(n => n.Id == id).FirstOrDefault();
+                Assert.Equal(null, notification);
+            }
+        }
     }
 }
